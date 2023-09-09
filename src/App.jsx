2faabb8a29a6dbe928828bd0b2287b0a2bb0104a
Farid Mia <mdfarid7830@gmail.com>
2023-09-09 17:48:03 +0600

    state

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Clock from './components/clock'
 import './App.css'
 
 function App() {
   
 
   return (
-      <h1>
-        hello world
-      </h1>
+      <div><Clock locale="bn-BD"/></div>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import App from './App';
 
-// function Clock({locale}) {
-//   return (
-//     <h1 className='heading'>
-//       <span className='text'>hello world {new Date().toLocaleTimeString(locale)}</span>
-//     </h1>
-//   );
-// }
+ReactDOM.render(
+  <React.StrictMode>
+      <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-class Clock extends React.Component {
-    render() {
-       
-      return (
-          <h1 className='heading'>
-            <span className='text'>hello world {this.props.children} {new Date().toLocaleTimeString(this.props.locale)}</span>
-          </h1>
-      );
-        
-    }
-}
-
-// ReactDOM.render(<Clock locale="bn-BD"/>, document.getElementById('root'));
-ReactDOM.render(<Clock locale="bn-BD">hello academy</Clock>, document.getElementById('root'));
+reportWebVitals(); // Call the reportWebVitals function
