a6245aace23970d782d05da003782e391edc7eaf
faridmia <mdfarid7830@gmail.com>
2024-12-21 23:01:08 +0600

    react use state and useEffect

---
+++ react-18/my-react-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import MyForm from './components/Form';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          <MyForm/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
