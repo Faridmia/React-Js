47a9f8565e899a230ecf822725be0d5dae8135e5
faridmia <mdfarid7830@gmail.com>
2025-06-29 23:50:36 +0600

    mount unmount

--- react-vs-nextjs/react-typescript/src/App.tsx
+++ react-vs-nextjs/react-typescript/src/App.tsx
@@ -1,5 +1,7 @@
 import "./App.css";
 import List2 from "./component/AddRemove";
+import CatFriends from "./component/CatFriends";
+import CatFriends2 from "./component/catview";
 import Demo from "./component/ConditionalRendering/Demo";
 import PackingList from "./component/ConditionalRendering/PackingList";
 import ContactFrom from "./component/ContactFrom";
@@ -14,6 +16,7 @@ import Darklight from "./component/dark-light-switcher/Darklight";
 import Form from "./component/Form";
 import FormSubmit from "./component/ManagingState/Answerfield";
 import Messenger from "./component/ManagingState/Parentcontact";
+import MountUnmountData from "./component/MountUnmount";
 import Form5 from "./component/objectupdate";
 import Profile from "./component/PropsComponent/Imagecomponent";
 import CounterAdvanced from "./component/Reducer";
@@ -24,10 +27,12 @@ import ProductList from "./component/RenderingList/ProductList";
 import RecipeApp from "./component/RenderingList/Recipe2";
 import RecipeList from "./component/RenderingList/Recipies";
 import Toolbar from "./component/ResponseEvent/Responseevent";
+import ShowRef from "./component/showref";
 import Gallery from "./component/StateMemory";
 import Counter2 from "./component/StateUpdater";
 import TaskApp from "./component/Taskapp/taskapp";
 import UserProfile from "./component/UserProfile";
+import VideoApp from "./component/VideoPlayer";
 export default function App() {
   return (
     <>
@@ -64,6 +69,11 @@ export default function App() {
         <ContextApifarid />
         <ProfilePage />
         <ImageContext />
+        <CatFriends />
+        <CatFriends2 />
+        <ShowRef />
+        <VideoApp />
+        <MountUnmountData />
       </div>
     </>
   );
