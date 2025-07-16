774ba5eb296ac7c135be3295c0d8f64c56c5cf68
faridmia <mdfarid7830@gmail.com>
2025-07-16 22:34:08 +0600

    redux react

--- react-vs-nextjs/redux-react/index.js
+++ react-vs-nextjs/redux-react/index.js
@@ -1,46 +1,68 @@
+// async action - api calling
+// api url - https://jsonplaceholder.typicode.com/todos
+// middleware - redux-thunk
+// axios api
+
+// REMOVE THIS LINE: import thunk from "redux-thunk";
+// It conflicts with your package.json's "type": "commonjs"
+
+const { default: axios } = require("axios");
 const { createStore, applyMiddleware } = require("redux");
-const { default: logger } = require("redux-logger");
+const thunk = require("redux-thunk"); // This is the correct way to import thunk in CommonJS
 
-// product reducer
-const GET_PRODUCTS = "GET_PRODUCTS";
-const ADD_PRODUCT = "ADD_PRODUCT";
+// constants
+const GET_TODOS_REQUEST = "GET_TODOS_REQUEST";
+const GET_TODOS_SUCCESS = "GET_TODOS_SUCCESS";
+const GET_TODOS_FAILED = "GET_TODOS_FAILED";
+const API_URL = "https://jsonplaceholder.typicode.com/todos";
 
-// cart const
-
-const initialProductState = {
-  products: ["sugar"],
-  numberofProducts: 1,
+// states
+const initialTodosState = {
+  todos: [],
+  isLoading: false,
+  error: null,
 };
 
-const initialCartState = {
-  cart: ["sugar", "salt"],
-  numberofProducts: 2,
-};
-
-const getProducts = () => {
+// actions
+const getTodosRequest = () => {
   return {
-    type: GET_PRODUCTS,
+    type: GET_TODOS_REQUEST,
   };
 };
 
-const addProduct = (product) => {
+const getTodosSuccess = (todos) => {
   return {
-    type: ADD_PRODUCT,
-    payload: product,
+    type: GET_TODOS_SUCCESS,
+    payload: todos,
   };
 };
 
-const productReducer = (state = initialProductState, action) => {
+const getTodosFailed = (error) => {
+  return {
+    type: GET_TODOS_FAILED,
+    payload: error,
+  };
+};
+
+// reducers
+const todosReducer = (state = initialTodosState, action) => {
   switch (action.type) {
-    case GET_PRODUCTS:
+    case GET_TODOS_REQUEST:
       return {
         ...state,
+        isLoading: true,
       };
-
-    case ADD_PRODUCT:
+    case GET_TODOS_SUCCESS:
       return {
-        products: [...state.products, action.payload],
-        numberofProducts: state.numberofProducts + 1,
+        ...state,
+        isLoading: false,
+        todos: action.payload,
+      };
+    case GET_TODOS_FAILED:
+      return {
+        ...state,
+        isLoading: false,
+        error: action.payload,
       };
 
     default:
@@ -48,13 +70,30 @@ const productReducer = (state = initialProductState, action) => {
   }
 };
 
-// store
+// async action creator
+const fetchData = () => {
+  return (dispatch) => {
+    dispatch(getTodosRequest());
+    axios
+      .get(API_URL)
+      .then((res) => {
+        const todos = res.data;
+        const title = todos.map((todo) => todo.title);
+        dispatch(getTodosSuccess(title));
+        console.log(title);
+      })
+      .catch((error) => {
+        const errorMessage = error.message;
+        dispatch(getTodosFailed(errorMessage));
+      });
+  };
+};
 
-const store = createStore(productReducer, applyMiddleware(logger));
+// store
+const store = createStore(todosReducer, applyMiddleware(thunk));
 
 store.subscribe(() => {
   console.log(store.getState());
 });
 
-store.dispatch(getProducts());
-store.dispatch(addProduct("banana"));
+store.dispatch(fetchData());
